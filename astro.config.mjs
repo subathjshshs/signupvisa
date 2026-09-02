0c446380fbf527001eca52476fa4d31dd64779bd
MD Afjal Hossan <133987003+subathjshshs@users.noreply.github.com>
2026-09-02 18:02:34 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     imageService: 'compile',
-    platformProxy: { enabled: true },
   }),
   vite: {
     plugins: [tailwindcss()],
