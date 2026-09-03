95352c39586db5213cd361b0303f60440c5be451
MD Afjal Hossan <133987003+subathjshshs@users.noreply.github.com>
2026-09-04 01:13:11 +0600

    Harden XML sitemap generation and route coverage

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,15 +4,27 @@ import { services } from '../data/services';
 
 const staticRoutes = ['/', '/services', '/destinations', '/about', '/contact', '/blog'];
 
+function escapeXml(value: string) {
+  return value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;');
+}
+
 export const GET: APIRoute = ({ site }) => {
   const base = (site?.toString() || 'https://signupvisa.com/').replace(/\/$/, '');
-  const urls = [
+  const paths = [
     ...staticRoutes,
     ...services.map((service) => `/services/${service.slug}`),
     ...destinations.map((destination) => `/destinations/${destination.slug}`),
   ];
-  const body = urls.map((path) => `  <url><loc>${base}${path}</loc></url>`).join('\n');
-  return new Response(`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${body}\n</urlset>`, {
-    headers: { 'Content-Type': 'application/xml; charset=utf-8', 'Cache-Control': 'public, max-age=3600, s-maxage=86400' },
-  });
+  const urls = [...new Set(paths)];
+  const body = urls.map((path) => `  <url><loc>${escapeXml(`${base}${path}`)}</loc></url>`).join('\n');
+
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${body}\n</urlset>`,
+    {
+      headers: {
+        'Content-Type': 'application/xml; charset=utf-8',
+        'Cache-Control': 'public, max-age=3600, s-maxage=86400',
+      },
+    },
+  );
 };
